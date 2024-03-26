61fd977223cc67943452a9f6dd076b63521a6e8c
Henry Ohanga <ohanga.henry@gmail.com>
2024-03-26 11:27:50 +0300

    Add support for C

---
+++ c/calculator.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function prototypes
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+
+int main()
+{
+    // Test cases
+    int num1 = 10;
+    int num2 = 3;
+
+    printf("Addition: %d\n", add(num1, num2));
+    printf("Subtraction: %d\n", subtract(num1, num2));
+    printf("Multiplication: %d\n", multiply(num1, num2));
+    printf("Division: %d\n", divide(num1, num2));
+
+    return 0;
+}
+
+int add(int a, int b)
+{
+    // Addition
+    return a + b;
+}
+
+int subtract(int a, int b)
+{
+    // Subtraction
+    return a - b;
+}
+
+int multiply(int a, int b)
+{
+    // Multiplication
+    return a * b;
+}
+
+int divide(int a, int b)
+{
+    // Division
+    return a / b;
+}
